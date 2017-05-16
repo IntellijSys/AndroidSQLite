a3b9bdb5fc10e18b38667fb500fce9ccc3b6d2c0
Mohammad Nurdin (Zero) <mohammadnrdn@gmail.com>
2017-05-17 03:03:28 +0800

    Update MainActivity.

--- app/src/main/java/my/intellij/androidsqlite/MainActivity.java
+++ app/src/main/java/my/intellij/androidsqlite/MainActivity.java
@@ -1,7 +1,10 @@
 package my.intellij.androidsqlite;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,26 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        DatabaseHandler db = new DatabaseHandler(this);
+
+        /**
+         * CRUD Operations
+         * */
+        // Inserting Tasks
+        Log.d("Insert: ", "Inserting ..");
+        db.addTask(new Task("Breakfast", "Nasi Lemak"));
+        db.addTask(new Task("Lunch", "Nasi Ayam"));
+        db.addTask(new Task("Dinner", "MCD"));
+
+        // Reading all tasks
+        Log.d("Reading: ", "Reading all tasks..");
+        List<Task> tasks = db.getAllTasks();
+
+        for (Task cn : tasks) {
+            String log = "TaskId: "+cn.getTaskId()+" ,Name: " + cn.getName() + " ,Description: " + cn.getDescription();
+            // Writing Tasks to log
+            Log.d("Name: ", log);
+        }
     }
 }
